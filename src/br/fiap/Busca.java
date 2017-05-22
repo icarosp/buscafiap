f8c1da35c4d7a162bf4a7ff62e226269cd06b756
logonrm <logonrm@fiap.com.br>
2017-05-22 15:29:36 -0300

    Alterando solucao p/ algoritmo Dijkstra

--- src/br/fiap/Busca.java
+++ src/br/fiap/Busca.java
@@ -1,7 +1,43 @@
 package br.fiap;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+
+/*Classe que defines os Nos*/
+class Vertex implements Comparable<Vertex>
+{
+    public final String name;
+    public Edge[] adjacencies;
+    public double minDistance = Double.POSITIVE_INFINITY;
+    public Vertex previous;
+    public Vertex(String argName) { name = argName; }
+    public String toString() { return name; }
+    public int compareTo(Vertex other)
+    {
+        return Double.compare(minDistance, other.minDistance);
+    }
+}
+
+/*Classe que defines os Arcos*/
+class Edge
+{
+   public final Vertex target;
+   public final double weight;
+   public Edge(Vertex argTarget, double argWeight)
+   { target = argTarget; weight = argWeight; }
+}
+
+
+
+
+
+
+
 
 /**
  * Enumeração que declara todas as possíveis ações,
@@ -180,6 +216,30 @@ public class Busca {
 
 	private No buscar() {
 		
+		Vertex source = new Vertex("Palma Sola");
+		
+		source.minDistance = 0.;
+	    
+		PriorityQueue<Vertex> vertexQueue = new PriorityQueue<Vertex>();
+	    vertexQueue.add(source);
+
+	    while (!vertexQueue.isEmpty()) {
+	        Vertex u = vertexQueue.poll();
+
+	            // Visit each edge exiting u
+	        for (Edge e : u.adjacencies){
+	            Vertex v = e.target;
+	            double weight = e.weight;
+	            double distanceThroughU = u.minDistance + weight;
+			    if (distanceThroughU < v.minDistance) {
+			       vertexQueue.remove(v);
+		
+			       v.minDistance = distanceThroughU ;
+			       v.previous = u;
+			       vertexQueue.add(v);
+			    }
+	        }
+	     }
 		/*No aberto = new No(estadoInicial, null, null);
 		fronteira.insere(aberto);
 		
@@ -197,8 +257,19 @@ public class Busca {
 			}
 		}*/
 		
-		return null;
+	return null;
 	}
+	
+    public static List<Vertex> getShortestPathTo(Vertex target)
+    {
+        List<Vertex> path = new ArrayList<Vertex>();
+        for (Vertex vertex = target; vertex != null; vertex = vertex.previous)
+            path.add(vertex);
+
+        Collections.reverse(path);
+        return path;
+    }
+	
 	
 	/**
 	 * Resolve o problema com base em busca, realizando
